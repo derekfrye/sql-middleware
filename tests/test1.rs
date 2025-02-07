2098e9d5e0d51f034eb238522acc547a6c05f897
Derek Frye <derek.frye@gmail.com>
2025-02-06 18:01:48 -0600

    fmt

--- tests/test1.rs
+++ tests/test1.rs
@@ -24,6 +24,7 @@ fn sqlite_mutltiple_column_test() -> Result<(), Box<dyn std::error::Error>> {
         let config_and_pool = ConfigAndPool::new_sqlite(x).await?;
         let pool = config_and_pool.pool.get().await?;
         let sqlite_conn = MiddlewarePool::get_connection(pool).await?;
+        
         let sconn = match &sqlite_conn {
             MiddlewarePoolConnection::Sqlite(sconn) => sconn,
             _ => panic!("Only sqlite is supported "),
--- tests/test2_postgres.rs
+++ tests/test2_postgres.rs
@@ -2,15 +2,23 @@
 // use sqlx_middleware::db::{ QueryState, DatabaseType, Db, ConfigAndPool };
 // use sqlx_middleware::model::{ CheckType, CustomDbRow, DatabaseResult, QueryAndParams, RowValues };
 use chrono::NaiveDateTime;
-use common::postgres::{setup_postgres_container, stop_postgres_container};
+use common::postgres::{ setup_postgres_container, stop_postgres_container };
 // use sqlx::{ Connection, Executor };
 
 use sql_middleware::middleware::{
-    ConfigAndPool, ConversionMode, CustomDbRow, MiddlewarePool, MiddlewarePoolConnection,
-    QueryAndParams, RowValues,
+    ConfigAndPool,
+    ConversionMode,
+    CustomDbRow,
+    MiddlewarePool,
+    MiddlewarePoolConnection,
+    QueryAndParams,
+    RowValues,
 };
 use sql_middleware::{
-    convert_sql_params, postgres_build_result_set, PostgresParams, SqlMiddlewareDbError,
+    convert_sql_params,
+    postgres_build_result_set,
+    PostgresParams,
+    SqlMiddlewareDbError,
 };
 
 use std::vec;
@@ -41,7 +49,8 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
     rt.block_on(async {
         // env::var("DB_USER") = Ok("postgres".to_string());
 
-        let stmt = "CREATE TABLE IF NOT EXISTS -- drop table event cascade
+        let stmt =
+            "CREATE TABLE IF NOT EXISTS -- drop table event cascade
                     test (
                     event_id BIGSERIAL NOT NULL PRIMARY KEY,
                     espn_id BIGINT NOT NULL,
@@ -66,7 +75,7 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
             }
         };
 
-        {
+        ({
             let tx = pgconn.transaction().await?;
             let result_set = {
                 let rs = tx.batch_execute(stmt).await?;
@@ -74,10 +83,10 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
             };
             tx.commit().await?;
             Ok::<_, SqlMiddlewareDbError>(result_set)
-        }?;
+        })?;
 
         let query = "DELETE FROM test;";
-        {
+        ({
             let tx = pgconn.transaction().await?;
             let result_set = {
                 let rs = tx.batch_execute(query).await?;
@@ -85,40 +94,31 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
             };
             tx.commit().await?;
             Ok::<_, SqlMiddlewareDbError>(result_set)
-        }?;
+        })?;
 
         let query_and_params = QueryAndParams {
             query: "INSERT INTO test (espn_id, name, ins_ts) VALUES ($1, $2, $3)".to_string(),
             params: vec![
                 RowValues::Int(123456),
                 RowValues::Text("test name".to_string()),
-                RowValues::Timestamp(NaiveDateTime::parse_from_str(
-                    "2021-08-06 16:00:00",
-                    "%Y-%m-%d %H:%M:%S",
-                )?),
+                RowValues::Timestamp(
+                    NaiveDateTime::parse_from_str("2021-08-06 16:00:00", "%Y-%m-%d %H:%M:%S")?
+                )
             ],
         };
 
-        {
-            let converted_params = convert_sql_params::<PostgresParams>(
-                &query_and_params.params,
-                ConversionMode::Query,
-            )?;
-            let tx = pgconn.transaction().await?;
-            tx.prepare(query_and_params.query.as_str()).await?;
-            let result_set = {
-                let rs = tx
-                    .execute(query_and_params.query.as_str(), &converted_params.as_refs())
-                    .await?;
+        let converted_params = convert_sql_params::<PostgresParams>(
+            &query_and_params.params,
+            ConversionMode::Execute
+        )?;
 
-                rs
-            };
-            tx.commit().await?;
-            Ok::<_, SqlMiddlewareDbError>(result_set)
-        }?;
+        let tx = pgconn.transaction().await?;
+        tx.prepare(query_and_params.query.as_str()).await?;
+        tx.execute(query_and_params.query.as_str(), &converted_params.as_refs()).await?;
+        tx.commit().await?;
 
         let query = "select * FROM test;";
-        let result = {
+        let result = ({
             let tx = pgconn.transaction().await?;
             let stmt = tx.prepare(query).await?;
             let result_set = {
@@ -127,31 +127,32 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
             };
             tx.commit().await?;
             Ok::<_, SqlMiddlewareDbError>(result_set)
-        }?;
+        })?;
 
         let expected_result = vec![CustomDbRow {
             column_names: vec![
                 "event_id".to_string(),
                 "espn_id".to_string(),
                 "name".to_string(),
-                "ins_ts".to_string(),
+                "ins_ts".to_string()
             ],
             rows: vec![
                 RowValues::Int(1),
                 RowValues::Int(123456),
                 RowValues::Text("test name".to_string()),
                 RowValues::Timestamp(
-                    NaiveDateTime::parse_from_str("2021-08-06 16:00:00", "%Y-%m-%d %H:%M:%S")
-                        .unwrap(),
-                ),
+                    NaiveDateTime::parse_from_str(
+                        "2021-08-06 16:00:00",
+                        "%Y-%m-%d %H:%M:%S"
+                    ).unwrap()
+                )
             ],
         }];
 
         let cols_to_actually_check = vec!["espn_id", "name", "ins_ts"];
 
         for (index, row) in result.results.iter().enumerate() {
-            let left: Vec<RowValues> = row
-                .column_names
+            let left: Vec<RowValues> = row.column_names
                 .iter()
                 .zip(&row.rows) // Pair column names with corresponding row values
                 .filter(|(col_name, _)| cols_to_actually_check.contains(&col_name.as_str()))
@@ -159,8 +160,7 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
                 .collect();
 
             // Get column names and row values from the expected result
-            let right: Vec<RowValues> = expected_result[index]
-                .column_names
+            let right: Vec<RowValues> = expected_result[index].column_names
                 .iter()
                 .zip(&expected_result[index].rows) // Pair column names with corresponding row values
                 .filter(|(col_name, _)| cols_to_actually_check.contains(&col_name.as_str()))
@@ -172,7 +172,7 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
 
         let query = "DROP TABLE test;
         DROP TABLE test_2;";
-        {
+        ({
             let tx = pgconn.transaction().await?;
             let result_set = {
                 let rs = tx.batch_execute(query).await?;
@@ -180,7 +180,7 @@ fn test2_postgres_cr_and_del_tbls() -> Result<(), Box<dyn std::error::Error>> {
             };
             tx.commit().await?;
             Ok::<_, SqlMiddlewareDbError>(result_set)
-        }?;
+        })?;
 
         // stop the container
 
